678b485cf56a829e4b06f2049751839a82b62a87
Kevin Tran <kevintraan@live.com>
2020-09-24 15:51:15 -0600

    Images, Button Examples

--- flutter_playground/lib/main.dart
+++ flutter_playground/lib/main.dart
@@ -19,36 +19,61 @@ class Home extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.greenAccent,
         ),
-        body: Center(
-          child: (Text(
-            "This is my playground to help me learn Flutter!",
-            // To style our text, we need a TextStyle widget
-            style: TextStyle(
-                // If we want to use a custom font, do the following:
-                // 1. First download one from google fonts and add it to the project
-                // 2. Then go to the pubpsec.yaml and follow steps to add font
-                // 3. Add to the fontFamily property
-                fontFamily: "Raleway",
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-                fontSize: 15.0,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 1.2),
-          )),
-        ),
+        body: Container(),
+
+
+
+
+            // Button Example:
+            // We need to have onPressed to listen to the event
+               // RaisedButton.icon(
+               //   onPressed: () {
+               //     print("Clicked.");
+               //   },
+               //   icon: Icon(Icons.mail),
+               //   label: Text("Mail"),
+               //   color: Colors.amber,
+               //
+               // ),
+
+
+
+
+            // Icons Example:
+            //Icon(Icons.local_airport, color: Colors.purple)
+
+            // Images Example:
+            // We can use NetWorkImage to grab an image straight from a URL
+            // A better way is to save the image and use AssetImage
+            // Refer to the pubspec.yaml to allow us to use asset images
+            // Image.asset("assets/oceanbeach.jpg")
+
+            // Text Example:
+            // (Text(
+            //   "This is my playground to help me learn Flutter!",
+            //   // To style our text, we need a TextStyle widget
+            //   style: TextStyle(
+            //       // If we want to use a custom font, do the following:
+            //       // 1. First download one from google fonts and add it to the project
+            //       // 2. Then go to the pubpsec.yaml and follow steps to add font
+            //       // 3. Add to the fontFamily property
+            //       fontFamily: "Raleway",
+            //       color: Colors.white,
+            //       fontStyle: FontStyle.italic,
+            //       fontSize: 15.0,
+            //       fontWeight: FontWeight.bold,
+            //       letterSpacing: 1.2),
+            // )),
+
+
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              title: Text("Home")
-    ),
+                icon: Icon(Icons.home), title: Text("Home")),
             BottomNavigationBarItem(
-              icon: Icon(Icons.settings),
-              title: Text("Settings")
-            )
+                icon: Icon(Icons.settings), title: Text("Settings"))
           ],
           selectedItemColor: Colors.amberAccent[400],
-
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},

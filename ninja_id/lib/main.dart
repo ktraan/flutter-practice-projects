e64776a8e8ffb39269661ff4eb30a467d8b64dd1
Kevin Tran <kevintraan@live.com>
2020-10-24 18:42:28 -0600

    Completed StatelessWidget app

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -19,12 +19,61 @@ class NinjaCard extends StatelessWidget {
       body: Padding(
         padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
         child: Column(
+          // This will apply alignment on the cross axis (X Axis)
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/chefturtol.jpg'),
+                radius: 40,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.greenAccent
+            ),
             Text("NAME", style: TextStyle(
               color: Colors.grey,
-              letterSpacing: 2
-
-            ),),
+              letterSpacing: 2),
+            ),
+            // This adds a box between widgets, good for spacing
+            SizedBox(height: 10.0),
+            Text("Kevin", style: TextStyle(
+              color: Colors.amberAccent[200],
+              letterSpacing: 2,
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Text("CURRENT NINJA LEVEL", style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2),
+            ),
+            SizedBox(height: 10.0),
+            Text("8", style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[400]
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'kevin@hotmail.com',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1
+                  )
+                )
+              ]
+            )
           ],
         ),
       )
